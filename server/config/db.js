2f7301e925c8d419bc430ef81b23194a46c3c593
vkhatri5 <vkhatri5@my.centennialcollege.ca>
2021-10-23 23:15:24 -0400

    first commit 2

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/book_store"
+    //"URI": "mongodb://localhost/book_store",
+    "URI": "mongodb+srv://Virat:9abWLAcnP2mAiMRi@mongodbserver.0ncr1.mongodb.net/book_store?retryWrites=true&w=majority"
 }
