5afbad87b5d8eae5073c1099856e62bbe58d55c5
vkhatri5 <vkhatri5@my.centennialcollege.ca>
2021-10-23 23:22:54 -0400

    first commit 3

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -76,9 +76,7 @@ module.exports.processLoginPage = (req, res, next) => {
                 email: user.email
             }
 
-            const authToken = jwt.sign(payload, DB.Secret, {
-                expiresIn: 604800 // 1 week
-            });
+            
 
             /* TODO - Getting Ready to convert to API
             res.json({success: true, msg: 'User Logged in Successfully!', user: {
